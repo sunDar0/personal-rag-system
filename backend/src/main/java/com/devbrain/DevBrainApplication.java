c61eac08886ca86baf31bcf341d17ef551d419c4
ted.gwon <ted.gwon@kakaovx.com>
2025-12-23 14:50:27 +0900

    [backend] feat: DevBrainApplication 클래스 추가 및 Spring Boot 애플리케이션 초기화

---
+++ backend/src/main/java/com/devbrain/DevBrainApplication.java
@@ -0,0 +1,13 @@
+package com.devbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevBrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevBrainApplication.class, args);
+    }
+}
+
